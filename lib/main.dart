e646f773d78efa81daa4da8a2fe1ce1dd4561513
hinut <si19003@edu.aomori-u.ac.jp>
2022-11-14 22:26:39 +0900

    GCPの同意画面設定、パッケージの導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:googleapis/calendar/v3.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:flutter/material.dart';
+import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sign_in_as_googleapis_auth.dart';
+import 'package:uuid/uuid.dart';
 
 void main() async{
   //アプリ実行前にFlutterアプリの機能を利用する場合に宣言
